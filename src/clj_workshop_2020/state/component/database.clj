428eb71164d5eaf5592303bd32283ac79abd1a9f
Jovanny Cruz <jovannypcg@gmail.com>
2020-07-22 21:31:02 -0500

    Add auxiliar function to start database connection

--- src/clj_workshop_2020/state/component/database.clj
+++ src/clj_workshop_2020/state/component/database.clj
@@ -1,6 +1,13 @@
 (ns clj-workshop-2020.state.component.database
   (:require [com.stuartsierra.component :as component]))
 
+(defn start-connection
+  [host port]
+  (do
+    (println "Starting database connection...")
+    (Thread/sleep 2000)
+    (atom {:host host :port port :data []})))
+
 ;; Database component is declared here.
 
 ;; ...
