5024bf2528585cd4fb9a6a268a669fd3fe461ab4
Alex Aldazabal <alex.aldazabal@nubank.com.br>
2020-08-06 17:14:48 -0500

    service initial structure

--- src/clj_workshop_2020/service.clj
+++ src/clj_workshop_2020/service.clj
@@ -4,27 +4,27 @@
             [datascript.core :as ds]
             [io.pedestal.http.route :as route]
             [mount.core :as mount]
+            [io.pedestal.http.body-params :as body-params]
             [clojure.data.json :as json]
             [io.pedestal.interceptor.error :as error-int]))
 
-(def path->name {})
+(def path->name {:name :enter})
 
-(def db-interceptor {})
+(def db-interceptor {:name :enter})
 
-(def body->json {})
+(def body->json {:name :leave})
 
 (def error-interceptor {})
-
-
 
 (def routes
   (route/expand-routes
     #{}))
 
+#_["/path" :get [handler] :route-name :name :constraints {:id ""}]
+
 #_(route/try-routing-for routes :prefix-tree "/greet/p001" :get)
 #_(route/try-routing-for routes :prefix-tree "/greet/p001" :get)
 #_(route/try-routing-for routes :prefix-tree "/greet/1" :get)
-
 
 #_(route/try-routing-for routes :prefix-tree "/heroes/Spider-Man" :get)
 #_(route/try-routing-for routes :prefix-tree "/heroes/Spider-Man/occupations" :get)
